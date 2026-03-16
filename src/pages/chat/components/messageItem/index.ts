af6ff614c8ffac79c7395313c02153feefa35c49
wenxin-99 <qq912305625@gmail.com>
2026-03-16 20:27:05 +0800

    Delete src directory

--- src/pages/chat/components/messageItem/index.ts
+++
@@ -1,5 +0,0 @@
-export { IntentConfirmSection } from './IntentConfirmSection';
-export { AssistantTaskCards, AssistantRegularContent } from './AssistantContent';
-export { UserContent } from './UserContent';
-export { MessageActions } from './MessageActions';
-export type { MessageItemProps, ContentProps } from './types';
--- src/pages/chat/exports.ts
+++
@@ -1,9 +0,0 @@
-// Chat 模块导出
-export { ChatHeader } from './components/ChatHeader';
-export { ChatMessages } from './components/ChatMessages';
-export { MessageItem } from './components/MessageItem';
-
-export { useChatReducer } from './hooks/useChatReducer';
-export { useChatMessages } from './hooks/useChatMessages';
-export { useFileUpload } from './hooks/useFileUpload';
-export { useThinkingMode } from './hooks/useThinkingMode';
